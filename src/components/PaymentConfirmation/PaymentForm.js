47510e1dc9803878199985fba4126dd1f6d3860d
Jean Frusca <jeang96@gmail.com>
2022-12-16 20:27:27 -0300

    feat: added full layout to the confirmation screen. A few input fixes left

---
+++ src/components/PaymentConfirmation/PaymentForm.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
+
+export default class PaymentForm extends React.Component {
+  state = {
+    cvc: '',
+    expiry: '',
+    focus: '',
+    name: '',
+    number: '',
+  };
+
+  handleInputFocus = (e) => {
+    this.setState({ focus: e.target.name });
+  };
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div id="PaymentForm">
+        <Cards
+          cvc={this.state.cvc}
+          expiry={this.state.expiry}
+          focused={this.state.focus}
+          name={this.state.name}
+          number={this.state.number}
+        />
+        <form>
+          <input
+            type="tel"
+            name="number"
+            placeholder="Número do cartão"
+            pattern="[\d| ]{16,22}"
+            onChange={this.handleInputChange}
+            onFocus={this.handleInputFocus}
+          />
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome"
+            onChange={this.handleInputChange}
+            onFocus={this.handleInputFocus}
+          />
+          <input
+            className="expiryDate"
+            type="tel"
+            name="expiry"
+            placeholder="Válido até"
+            pattern="\d\d/\d\d"
+            onChange={this.handleInputChange}
+            onFocus={this.handleInputFocus}
+          />
+
+          <input
+            className="CVC"
+            type="tel"
+            name="cvc"
+            placeholder="CVC"
+            pattern="\d{3,4}"
+            onChange={this.handleInputChange}
+            onFocus={this.handleInputFocus}
+          />
+        </form>
+      </div>
+    );
+  }
+}
--- src/components/PaymentConfirmation/index.js
+++ src/components/PaymentConfirmation/index.js
@@ -1,23 +1,30 @@
 import { Typography } from '@material-ui/core';
 import { useState } from 'react';
 import styled from 'styled-components';
+import Button from '../Form/Button';
+import PaymentForm from './PaymentForm';
 
 export default function PaymentConfirmation() {
   const [ticketType, setTicketType] = useState({
     isOnline: false,
     hasHotel: true,
   });
-  let onlineText = '';
-  let hotelText = '';
+
   return (
     <>
       <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
       <SubTitle variant="h6">Ingresso escolhido</SubTitle>
       <TicketChosenContainer ticketType={ticketType}>
-        {ticketType.isOnline ? (onlineText = 'Online') : (onlineText = 'Presencial')} +
-        {ticketType.hasHotel ? (hotelText = ' Com Hotel') : (hotelText = ' Sem Hotel')}
+        {ticketType.isOnline ? 'Online' : 'Presencial'} +{ticketType.hasHotel ? ' Com Hotel' : ' Sem Hotel'}
       </TicketChosenContainer>
       <SubTitle variant="h6">Pagamento</SubTitle>
+
+      <PaymentFormContainer>
+        <PaymentForm></PaymentForm>
+      </PaymentFormContainer>
+      <SubmitContainer>
+        <Button type="submit">Finalizar Pagamento</Button>
+      </SubmitContainer>
     </>
   );
 }
@@ -39,4 +46,44 @@ const TicketChosenContainer = styled.div`
   border-radius: 20px;
   background-color: #ffeed2;
   margin-bottom: 20px;
+`;
+
+const SubmitContainer = styled.div`
+  margin-top: 40px !important;
+  width: 100% !important;
+
+  > button {
+    margin-top: 0 !important;
+  }
+`;
+
+const PaymentFormContainer = styled.div`
+  width: 100%;
+  #PaymentForm {
+    margin-left: 0px;
+    display: flex;
+    justify-content: flex-start;
+    > form {
+      margin-left: 20px;
+
+      > input {
+        margin: 5px;
+        border-radius: 5px;
+        border: solid #8e8e8e 1px;
+        height: 20%;
+        width: 55%;
+      }
+      > input::placeholder {
+        text-align: start;
+        font-size: 16px;
+        padding: 3px 0px 3px 10px;
+      }
+      .expiryDate {
+        width: 30%;
+      }
+      .CVC {
+        width: 15%;
+      }
+    }
+  }
 `;
