c69398e77545c21591b51470e9830c49464ce90b
Gustavo Vasques <gvasques.dev@gmail.com>
2022-12-23 17:30:44 -0300

    feat: adding hotel screen components

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,41 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import useTicketPaid from '../../../hooks/api/useTicketPaid';
+import UnauthourizedPayment from './unauthorizedPayment';
+import UnauthourizedTicketType from './unauthorizedTicketType';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const { ticketData }  = useTicketPaid();
+  console.log(ticketData);
+
+  if (ticketData?.status === 'RESERVED') {
+    return (<>
+      <StyledTypography variant="h4">Escolha de Hotel e quarto</StyledTypography>
+      <HotelsAndRoomsContainer><UnauthourizedPayment/></HotelsAndRoomsContainer>
+    </>); 
+  } else if (ticketData?.ticketTypeId === 2 || ticketData?.ticketTypeId === 3 || ticketData?.ticketTypeId === 4) {
+    return (<>
+      <StyledTypography variant="h4">Escolha de Hotel e quarto</StyledTypography>
+      <HotelsAndRoomsContainer><UnauthourizedTicketType/></HotelsAndRoomsContainer>
+    </>); 
+  } else {
+    return (<>
+      <StyledTypography variant="h4">Escolha de Hotel e quarto</StyledTypography>
+      <HotelsAndRoomsContainer>Em breve</HotelsAndRoomsContainer>
+    </>);
+  }
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
+
+const HotelsAndRoomsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80%;
+  margin-bottom: 40px;
+`;
