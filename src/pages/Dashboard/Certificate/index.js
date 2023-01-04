a50665841a4e6809d0b22b740353de9a2ff9151f
HannahEbb <hannah.ebb.he@gmail.com>
2023-01-04 20:18:53 -0300

    test: last alterations

--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -1,3 +1,20 @@
+import { useEffect, useState } from 'react';
+import ActivityContext from '../../../contexts/ActivitiesInfoContext';
+import useActivities from '../../../hooks/api/useActivities';
+
 export default function Certificate() {
-  return 'Certificado: Em breve!';
+  const { activityData } = useActivities();
+  const [show, setShow] = useState('fail!');
+
+  useEffect(() => {
+    if (activityData) {
+      setShow('funcionando!');
+    }
+  });
+
+  return (
+    <>
+      <p>{show}</p>
+    </>
+  );
 }
